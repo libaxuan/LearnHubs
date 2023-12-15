6c09b658d5cd99f6d3ca97ae2816f1bd25fda3e0
libaxuan <33255877+libaxuan@users.noreply.github.com>
2023-12-15 10:54:37 +0000

    Deploying to lh-pages from @ libaxuan/LearnHubs@9bb82b6aa7291e4f19df82ed3158e03a0fc43c55 🚀

---
+++ assets/pageview-92bd2153.js
@@ -0,0 +1,1 @@
+import{a5 as n}from"./app-91ee3df5.js";const r=async()=>{try{const{pageviewCount:t}=await n(()=>import("./app-91ee3df5.js").then(e=>e.a6),[]);return t({serverURL:"https://waline.gpt-vip.top"})}catch{console.error("@waline/client is not installed!");return}};export{r as updatePageview};
